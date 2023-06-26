55c279929340e2c4a7b400de34c11c73ebbd2d11
D Harshil Kumar Reddy <114159242+Harshil-Kumar@users.noreply.github.com>
2023-06-26 13:05:18 +0530

    Add files via upload

---
+++ lang_connection.js
@@ -0,0 +1,59 @@
+const calculateButton = document.getElementById('search-btn');
+const resultElement = document.getElementById('result');
+const urlParams = new URLSearchParams(window.location.search);
+const selectedOption = urlParams.get('option');
+
+// Use the selected option as needed
+console.log('Selected option:', selectedOption);
+// Function to calculate the word difference using OpenAI API
+async function calculateDifference() {
+  const input1 = document.getElementById('input1').value;
+  const input2 = document.getElementById('input2').value;
+
+  const difference = await getWordDifference(input1, input2);
+
+  // Display the difference in the result element
+  resultElement.textContent = difference;
+}
+
+// Function to get the word difference using OpenAI API
+async function getWordDifference(input1, input2) {
+  const finalSearchTerm = "What is the conection between " + input1 + " and " + input2 + "? Give me the answer in "+ selectedOption +" language" ;
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer openai-api-key',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: finalSearchTerm }],
+      max_tokens: 2000
+    })
+  };
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+    const data = await response.json();
+
+    // Extract the message content from the response
+    const message = data.choices[0].message.content;
+
+    return message;
+  } catch (error) {
+    console.error(error);
+    return 'Error: Failed to calculate the difference.';
+  }
+}
+
+calculateButton.addEventListener('click', calculateDifference);
+
+const languageSelect = document.getElementById('language-select');
+
+languageSelect.addEventListener('change', function() {
+    const selectedOption = languageSelect.value;
+  
+    // Redirect to the language.html page with the selected option as a query parameter
+    window.location.href = `lang_connection.html?option=${selectedOption}`;
+  });
---
+++ language.js
@@ -0,0 +1,95 @@
+const submitButton = document.getElementById('submit-btn');
+const resultElement = document.getElementById('result');
+const previousSearchesList = document.getElementById('previous-searches-list');
+// Get the selected option from the URL query parameter
+const urlParams = new URLSearchParams(window.location.search);
+const selectedOption = urlParams.get('option');
+
+// Use the selected option as needed
+console.log('Selected option:', selectedOption);
+
+
+// Function to handle the form submission
+async function getMessage(e) {
+  e.preventDefault();
+  
+  const searchTerm = document.getElementById('search-bar').value;
+  const finalSearchTerm = "Explain the following concept: " + searchTerm+ "totally in" +selectedOption+ "language";
+
+  // Create a new list item element for the search query
+  const listItem = document.createElement('li');
+  listItem.textContent = searchTerm;
+  listItem.classList.add('search-item');
+  listItem.addEventListener('click', function() {
+    searchConcept(searchTerm);
+  });
+
+  // Append the new list item to the previous searches list
+  previousSearchesList.insertBefore(listItem, previousSearchesList.firstChild);
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer openai-api-key',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: finalSearchTerm }],
+      max_tokens: 2000
+    })
+  };
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+    const data = await response.json();
+
+    // Extract the message content from the response
+    const message = data.choices[0].message.content;
+
+    // Update the content of the result element with the message
+    resultElement.textContent = message;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+submitButton.addEventListener('click', getMessage);
+
+// Function to search a concept
+function searchConcept(searchTerm) {
+  document.getElementById('search-bar').value = searchTerm;
+  submitButton.click();
+}
+
+// Function to open the "Difference between Concepts" page
+function openDifferencePage() {
+  window.open('lang_difference.html', '_blank');
+}
+
+// Add event listener to the "Difference" button
+const differenceButton = document.getElementById('difference-btn');
+differenceButton.addEventListener('click', openDifferencePage);
+
+// Function to open the "Connection between Concepts" page
+function openConnectionPage() {
+  window.open('lang_connection.html', '_blank');
+}
+
+// Add event listener to the "Connection" button
+const connectionButton = document.getElementById('connection-btn');
+connectionButton.addEventListener('click', openConnectionPage);
+
+const languageSelect = document.getElementById('language-select');
+
+languageSelect.addEventListener('change', function() {
+    const selectedOption = languageSelect.value;
+  
+    // Redirect to the language.html page with the selected option as a query parameter
+    window.location.href = `language.html?option=${selectedOption}`;
+  });
+
+
+
+
+
